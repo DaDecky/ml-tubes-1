5acf8656aa251818487d81db3514113ab07bf3da
KarRz <reza11781@gmail.com>
2026-03-16 12:32:40 +0700

    fix: correct time tracking in Adam optimizer

--- src/ffnn/optimizers.py
+++ src/ffnn/optimizers.py
@@ -21,21 +21,24 @@ class Adam(Optimizer):
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
-        self.t = 0
+        self.t = {}
         self.m_w = {}
         self.v_w = {}
         self.m_b = {}
         self.v_b = {}
 
     def update(self, layer, dW, dB):
-        self.t += 1
         key = id(layer)
 
         if key not in self.m_w:
+            self.t[key] = 0
             self.m_w[key] = np.zeros_like(dW)
             self.v_w[key] = np.zeros_like(dW)
             self.m_b[key] = np.zeros_like(dB)
             self.v_b[key] = np.zeros_like(dB)
+        
+        self.t[key] += 1
+        t_current = self.t[key]
 
         self.m_w[key] = self.beta1 * self.m_w[key] + (1 - self.beta1) * dW
         self.v_w[key] = self.beta2 * self.v_w[key] + (1 - self.beta2) * (dW ** 2)
@@ -43,11 +46,11 @@ class Adam(Optimizer):
         self.m_b[key] = self.beta1 * self.m_b[key] + (1 - self.beta1) * dB
         self.v_b[key] = self.beta2 * self.v_b[key] + (1 - self.beta2) * (dB ** 2)
 
-        m_w_hat = self.m_w[key] / (1 - self.beta1 ** self.t)
-        v_w_hat = self.v_w[key] / (1 - self.beta2 ** self.t)
+        m_w_hat = self.m_w[key] / (1 - self.beta1 ** t_current)
+        v_w_hat = self.v_w[key] / (1 - self.beta2 ** t_current)
 
-        m_b_hat = self.m_b[key] / (1 - self.beta1 ** self.t)
-        v_b_hat = self.v_b[key] / (1 - self.beta2 ** self.t)
+        m_b_hat = self.m_b[key] / (1 - self.beta1 ** t_current)
+        v_b_hat = self.v_b[key] / (1 - self.beta2 ** t_current)
 
         layer._weights -= self.lr * m_w_hat / (np.sqrt(v_w_hat) + self.epsilon)
         layer._bias -= self.lr * m_b_hat / (np.sqrt(v_b_hat) + self.epsilon)
